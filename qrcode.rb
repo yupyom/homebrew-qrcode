35966841f7fabb4ac2431f942f78d308b580e7cf
yupyom <yupyom.log@gmail.com>
2016-10-06 21:09:18 +0900

    Update qrcode.rb

--- qrcode.rb
+++ qrcode.rb
@@ -2,10 +2,11 @@ class Qrcode < Formula
   desc "command line tool of qrcode generator."
   homepage ""
   url "https://github.com/yupyom/homebrew-tools/releases/download/v1.0.0/qrcode-1.0.0-darwin-amd64.zip"
-  sha256 "d963b7fe503b5a62fd9b0cd1497a5068860bb506e76bb34acb703dfab9da421e"
+  sha256 "f524a536d5bab710d45687f40a29255dcb463e8fe7c969e7be88f38c1137b71c"
   version "1.0.0"
 
   def install
     bin.install "qrcode"
+    man.install "qrcode.1"
   end
 end
