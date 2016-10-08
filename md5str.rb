79bf9166d42028059a36c2fda8e9a66942d701fb
yupyom <yupyom.log@gmail.com>
2016-10-09 02:50:48 +0900

    Update md5str.rb

--- md5str.rb
+++ md5str.rb
@@ -1,6 +1,6 @@
 class Md5str < Formula
   desc "command line tool of MD5 checksum string generator."
-  homepage ""
+  homepage "http://yupyom.github.io/tools/md5str.1.html"
   url "https://github.com/yupyom/homebrew-tools/releases/download/v1.0.0/md5str-1.0.0-darwin-amd64.zip"
   sha256 "54ffb1f712aba43d5bf2447f18ab7509b6b4cd0d115d80fb92df197d540c36c0"
   version "1.0.0"
