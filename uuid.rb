96a1698db3a40c6d68a595a09fff76d52f715c31
yupyom <yupyom.log@gmail.com>
2016-10-09 03:12:12 +0900

    Update uuid.rb

--- uuid.rb
+++ uuid.rb
@@ -2,7 +2,7 @@ class Uuid < Formula
   desc "command line tool of UUID version 4 string generator."
   homepage "http://yupyom.github.io/tools/uuid.1.html"
   url "https://github.com/yupyom/homebrew-tools/releases/download/v1.0.0/uuid-1.0.0-darwin-amd64.zip"
-  sha256 "bdd070d3479459e8879b681731f607b0023a4859b6bc10799249a2ea44523173"
+  sha256 "c061d5c317e864f6a512674d68bdbe9c2f37257623495b212290f5cbddc2f251"
   version "1.0.0"
 
   def install
