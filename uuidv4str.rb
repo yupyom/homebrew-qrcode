bc6f9b5bb684eba9e9c5d1b70a9a44e20fbe796b
yupyom <yupyom.log@gmail.com>
2016-10-09 03:34:45 +0900

    Update uuidv4str.rb

--- uuidv4str.rb
+++ uuidv4str.rb
@@ -2,7 +2,7 @@ class Uuidv4str < Formula
   desc "command line tool of UUID version 4 string generator."
   homepage "http://yupyom.github.io/tools/uuidv4str.1.html"
   url "https://github.com/yupyom/homebrew-tools/releases/download/v1.0.0/uuidv4str-1.0.0-darwin-amd64.zip"
-  sha256 "a8c342c266e8732d4d12bc7dd642133baf2f77c8fa019fb5cbfa9fe10b2f0350"
+  sha256 "dfed6ffcc051ac8008dc3cf1737b6783fc785e380fe72f5bc1e23394e56fe74a"
   version "1.0.0"
 
   def install
