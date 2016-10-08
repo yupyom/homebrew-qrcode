50bee710d108f1a5717b9d6315da3965ba75b75d
yupyom <yupyom.log@gmail.com>
2016-10-09 03:16:51 +0900

    Update uuid.rb

--- uuid.rb
+++ uuid.rb
@@ -3,7 +3,7 @@ class Uuid < Formula
   homepage "http://yupyom.github.io/tools/uuid.1.html"
   url "https://github.com/yupyom/homebrew-tools/releases/download/v1.0.1/uuid-1.0.1-darwin-amd64.zip"
   sha256 "6cfc44edf8de824067a573831d382da75304aac4048e188a84996ac9763d576d"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "uuid"
